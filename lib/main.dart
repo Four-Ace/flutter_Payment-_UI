2180bb63ee225873b6e24a232bb577f71e9f9e16
SJ Pyo <skajtdltdj2@gmail.com>
2023-04-21 23:14:24 +0900

    v1 - added menu bar icon - greeting messages

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,54 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const App());
 }
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFF181818),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(
+                    Icons.menu,
+                    color: Colors.white.withOpacity(0.8),
+                    size: 60,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Text(
+                        'Hola, Amiga♥',
+                        style: TextStyle(color: Colors.white, fontSize: 28),
+                      ),
+                      Text(
+                        'Buneos dias!',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.8),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
