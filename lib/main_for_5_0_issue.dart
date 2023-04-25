8796cf25c0e25669b1ffd910cf8d4f0e3202110a
SJ Pyo <skajtdltdj2@gmail.com>
2023-04-25 20:56:32 +0900

    v8'' Make extra .dart files for share for nomadcoder.com
    v8 was ...
    - Timer Code Challenge
    
    - Made resetTimer function. It used when Timer are done and pushed reset button.
    - Use 3 sizedBox with column.
    - Covered Icons with sizedBox.
    - Using spaceAround, alignment boxes.

---
+++ lib/main_for_5_0_issue.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Column(
+        children: [
+          Flexible(
+            flex: 1,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                '25:00',
+                style: TextStyle(
+                  color: Theme.of(context).cardColor,
+                  fontSize: 89,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 3,
+            child: Center(
+              //
+              // this part was problem
+              child: SizedBox(
+                width: 150,
+                height: 150,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.play_circle_outline,
+                    color: Theme.of(context).cardColor,
+                    size: 120,
+                  ),
+                ),
+              ),
+              //treated it!
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.vertical(
+                        top: Radius.circular(50),
+                      ),
+                      color: Theme.of(context).cardColor,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Pomodors',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color:
+                                Theme.of(context).textTheme.displayLarge!.color,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Text(
+                          '0',
+                          style: TextStyle(
+                            fontSize: 58,
+                            color:
+                                Theme.of(context).textTheme.displayLarge!.color,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
